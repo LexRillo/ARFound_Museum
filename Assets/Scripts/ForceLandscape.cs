b37d9b2582bc767075422261cbf527c03d9b26fa
LexRillo <fearsless@hotmail.it>
2020-08-03 15:11:07 +0100

    For checking if the camera has permissions

--- Assets/Scripts/ForceLandscape.cs
+++ Assets/Scripts/ForceLandscape.cs
@@ -8,6 +8,16 @@ public class ForceLandscape : MonoBehaviour
     void Start()
     {
         Screen.orientation = ScreenOrientation.Landscape;
+
+        Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            Debug.Log("Camera found and authorized");
+        }
+        else
+        {
+            Debug.Log("Camera not found or not authorized");
+        }
     }
 
     // Update is called once per frame
